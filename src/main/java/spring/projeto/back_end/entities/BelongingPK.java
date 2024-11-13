7f55a0814c198f091223250b95df6941be9bc419
Gustavo-Henrique01 <gusta.2019.henrique@gmail.com>
2024-11-13 18:04:25 -0300

    modelo de dominio

---
+++ src/main/java/spring/projeto/back_end/entities/BelongingPK.java
@@ -0,0 +1,66 @@
+package spring.projeto.back_end.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable//encapsulamento do game_id e list_id
+public class BelongingPK {
+	
+	@ManyToOne
+	@JoinColumn(name= "game_id")
+	private Game Game ;
+	
+	@ManyToOne
+	@JoinColumn(name= "list_id")
+	private GameList list ;
+	
+	
+	public BelongingPK(GameList list, spring.projeto.back_end.entities.Game game) {
+		super();
+		this.list = list;
+		Game = game;
+	}
+
+	public BelongingPK() {
+	
+	}
+
+	public GameList getList() {
+		return list;
+	}
+
+	public void setList(GameList list) {
+		this.list = list;
+	}
+
+	public Game getGame() {
+		return Game;
+	}
+
+	public void setGame(Game game) {
+		Game = game;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Game, list);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BelongingPK other = (BelongingPK) obj;
+		return Objects.equals(Game, other.Game) && Objects.equals(list, other.list);
+	}
+	
+	
+}
